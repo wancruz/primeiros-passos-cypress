44cd1f8a9afd5f6f68e18a1adfe6eedbe0ba1335
Wanderson Willian Cruz <100000005+wancruz@users.noreply.github.com>
2025-05-01 18:21:51 -0300

    pages created for separation

--- cypress/e2e/user.spec.cy.js
+++ cypress/e2e/user.spec.cy.js
@@ -1,44 +1,53 @@
-import userData from '../fixtures/users/user-data.json'
+import userData from '../fixtures/users/user-data.json';
+import LoginPage from '../pages/loginPage';
+
+const loginPage = new LoginPage()
+
 
 describe('Orange HRM Tests', () => {
 
  const selectorsList = {
-  usernameField: "[name='username']",
-  passwordField: "[name='password']",
-  loginButton: "[type='submit']",
+  
+  
   sectionTitleTopBar: ".oxd-topbar-header-breadcrumb .oxd-topbar-header-breadcrumb-module",
   dashboardGrid: ".orangehrm-dashboard-grid",
-  wrongCredentialAlert: "[role='alert']",
+ 
   myInfoButton: "[href='/web/index.php/pim/viewMyDetails']",
   firstNameField: "[name='firstName']",
   middleName: "[name='middleName']",
   genericField: ".oxd-input--active",
   dataField: ":nth-child(2) > .oxd-input-group > :nth-child(2) > .oxd-date-wrapper > .oxd-date-input > .oxd-input",
   dataCloseButton: ".--close",
-  submitButton: "[type='submit']"
+  submitButton: "[type='submit']",
+  genericComboBox: ".oxd-select-text-input"
 
  }  
 
   it.only('User Info Update - Success', () => {  // it.skip
-    cy.visit('/auth/login')
-    cy.get(selectorsList.usernameField).type(userData.userSuccess.username)
-    cy.get(selectorsList.passwordField).type(userData.userSuccess.password)
-    cy.get(selectorsList.loginButton).click()
-    cy.location('pathname').should('equal', '/web/index.php/dashboard/index')
-    cy.get(selectorsList.dashboardGrid)
-    cy.get(selectorsList.myInfoButton).click()
-    cy.get(selectorsList.firstNameField).clear().type("FirstNameTest")
-    cy.get(selectorsList.middleName).clear().type("MiddleNameest")
-    cy.get(selectorsList.genericField).eq(2).clear().type("Lastname")
-    // cy.get(selectorsList.genericField).eq(3).clear().type("NickName")
-    cy.get(selectorsList.genericField).eq(3).clear().type("EmployeeId")
-    cy.get(selectorsList.genericField).eq(4).clear().type("OtherId")
-    cy.get(selectorsList.genericField).eq(5).clear().type("Driver")
-    cy.get(selectorsList.dataField).clear().type("2025-04-21")
-    cy.get(selectorsList.dataCloseButton).click()
-    cy.get(selectorsList.submitButton).eq(0).click()
-    cy.get('body').should('contain', 'Successfully Updated')
-    cy.get('.oxd-toast-close')
+    loginPage.accessLoginPage()
+    loginPage.loginWithUser(userData.userSuccess.username,userData.userSuccess.password)
+    // cy.get(selectorsList.loginButton).click()
+    // cy.location('pathname').should('equal', '/web/index.php/dashboard/index')
+    // cy.get(selectorsList.dashboardGrid)
+    // cy.get(selectorsList.myInfoButton).click()
+    // cy.get(selectorsList.firstNameField).clear().type("FirstNameTest")
+    // cy.get(selectorsList.middleName).clear().type("MiddleNameest")
+    // cy.get(selectorsList.genericField).eq(2).clear().type("Lastname")
+    // // cy.get(selectorsList.genericField).eq(3).clear().type("NickName")
+    // cy.get(selectorsList.genericField).eq(3).clear().type("EmployeeId")
+    // cy.get(selectorsList.genericField).eq(4).clear().type("OtherId")
+    // cy.get(selectorsList.genericField).eq(5).clear().type("Driver")
+    // cy.get(selectorsList.dataField).clear().type("2025-04-21")
+    // cy.get(selectorsList.dataCloseButton).click()
+
+    // cy.get(selectorsList.genericComboBox).eq(0).click()
+    // cy.contains('American').click()
+    // cy.get(selectorsList.genericComboBox).eq(1).click()
+    // cy.contains('Single').click()
+
+    // cy.get(selectorsList.submitButton).eq(0).click()
+    // cy.get('body').should('contain', 'Successfully Updated')
+    // cy.get('.oxd-toast-close')
   })
 
   it('Login - Fail', () => {
